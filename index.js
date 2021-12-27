fb54649436ae402842cf47df539963d620a5abe4
Che <cgoranov@gmail.com>
2021-12-26 20:40:44 -0600

    commit tree 544cd32337f304028aecf492f5f7520717deb0d4

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+let num3 = 16
+let num4 = 6
+
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 100)
+
+let mod = num3 % num4
+
+const max = Math.max( 5, 10, 15, 20 );
